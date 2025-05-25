c7852b043d634807b6511f532ecc5ab57b126c98
Mohamed Ahmed Bakr <142639226+mohamedbakr2005@users.noreply.github.com>
2025-05-25 11:23:55 +0300

    first edit

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
   static const Color primary = Color(0xFF247CFF);
   static const Color grey = Color(0xFF757575);
+  static const Color red = Colors.red;
 }
